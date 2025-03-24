9758218437af9f9a978df66cecedc13835fba994
SihomaSa <sandy.ochoa.27@unsch.edu.pe>
2025-03-24 16:55:36 -0500

    Nuevo comentario

--- assets/js/pages/rodape/Rodape.js
+++ assets/js/pages/rodape/Rodape.js
@@ -5,6 +5,6 @@ export function Rodape() {
   var root = document.getElementById('root')
   var footer = montarElemento('footer', 'rodape')
   footer.innerHTML = `<p class="rodape__texto">©Desarrollado por ${infos.nome}</p>
-  <p class="rodape__texto">2022</p>`
+  <p class="rodape__texto">2025</p>`
   root.appendChild(footer);
 }
--- assets/js/pages/sobre/Sobre.js
+++ assets/js/pages/sobre/Sobre.js
@@ -13,7 +13,7 @@ export function Sobre() {
   paragrafo1.textContent = 'Empecé mis estudios con programación a inicios de 2022. Comenzando con HTML y CSS, desarrollado en un curso de la universidad. Esto despertó mi interés por el desarrollo.'
 
   paragrafo2.textContent = 'En agosto del 2022, gané dos becas para ser desarrolladora FullStack y FrontEnd en Becas Bicentenario y en Alura ONE Education respectivamente, estoy aprendiendo más sobre HTML, CSS, Javascript y JAVA, Spring, Angular y entre otros. Con mucho esmero espero lograr la meta  y trabajar como desarrolladora.';
-  paragrafo3.textContent = "SSOC"
+  paragrafo3.textContent = "GRLP"
 
   section.appendChild(h2)
   section.appendChild(paragrafo1)
